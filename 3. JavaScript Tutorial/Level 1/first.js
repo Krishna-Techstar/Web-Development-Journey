feaa3d5ddad0cb1b9fe7f435abf79368a3e03f35
Krishnakant Sharma <139650964+Krishna-Techstar@users.noreply.github.com>
2025-06-29 02:22:27 +0530

    Level 1

---
+++ 3. JavaScript Tutorial/Level 1/first.js
@@ -0,0 +1,17 @@
+console.log("hi i am krsihna");
+console.log("hello everyone this is learner krishna ");
+fullname = "krishnakant_sharma";
+age = 20;
+price = 99.99;
+radius= 16;
+diameter= 32;
+
+// non primitive data type
+
+const student = {
+    fullName : "Krishnakant Sharma",
+    age : 20,
+    sgpa : 9.24,
+    isPass : true,
+};
+console.log( student["age"]);
