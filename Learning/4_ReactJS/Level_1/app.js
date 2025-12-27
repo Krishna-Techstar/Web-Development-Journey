06000579bb3f6cee89963fbd2fd5674d24a465ce
Krishnakant Sharma <139650964+Krishna-Techstar@users.noreply.github.com>
2025-12-27 18:05:58 +0530

    Started With React Development
    Learn basic concept basically revise and then about import export to create new react folder

---
+++ Learning/4_ReactJS/Level_1/app.js
@@ -0,0 +1,8 @@
+// const a = 19
+// export default a
+
+export const arr =[10, 20 , 30, 40, 50 ]
+
+const user = 'Krishnakant Sharma'
+
+export default user
---
+++ Learning/4_ReactJS/Level_1/script.js
@@ -0,0 +1,12 @@
+import user from './app.js'
+import {arr} from './app.js'
+
+var h1 = document.createElement('h1')
+h1.innerHTML = ' Hello Form JavaScript'
+document.body.appendChild(h1)
+
+
+console.log(user)
+
+console.log(arr)
+
